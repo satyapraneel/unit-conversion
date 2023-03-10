60c5a4e2910c793503c3a0a80d5e062a4a1d9fa9
Satyapraneel Holla <satyapraneel.holla@landmarkgroup.in>
2023-03-10 22:45:25 +0530

    First commit

---
+++ src/UnitConversionClass.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace UnitConversion;
+
+class UnitConversionClass
+{
+}
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace UnitConversion\Tests;
 
 use PHPUnit\Framework\TestCase;
 
