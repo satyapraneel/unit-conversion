896b4bed7773786f94603c9170a3116dbccf2c2e
satyapraneel <satyapraneel@users.noreply.github.com>
2023-03-12 14:13:21 +0000

    Fix styling

--- src/Weight.php
+++ src/Weight.php
@@ -4,18 +4,16 @@ namespace UnitConversion;
 
 class Weight
 {
-
-    public static function fromKilogram(float $kilograms) : self
+    public static function fromKilogram(float $kilograms): self
     {
         return new static($kilograms);
     }
 
     public function __construct(private float $kilogram)
     {
-
     }
 
-    public function toLbs() : float
+    public function toLbs(): float
     {
         return number_format($this->kilogram * 2.204623, 4);
     }
